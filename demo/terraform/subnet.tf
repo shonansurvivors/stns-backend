1790d4f2feccb10c1026590f52ac4cef26843959
shonansurvivors <shonansurvivors@gmail.com>
2020-12-28 17:31:49 +0900

    create route_table

--- demo/terraform/subnet.tf
+++ demo/terraform/subnet.tf
@@ -1,3 +1,6 @@
+/*
+public subnet and more
+*/
 resource "aws_subnet" "public" {
   count = 2
 
@@ -11,6 +14,30 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.system_name}-${var.env_name}-public-route-table"
+  }
+}
+
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public[count.index].id
+}
+
+/*
+private subnet and more
+ */
 resource "aws_subnet" "private" {
   count = 2
 
@@ -22,4 +49,29 @@ resource "aws_subnet" "private" {
   tags = {
     Name = "${var.system_name}-${var.env_name}-private-subnet-${count.index}"
   }
+}
+
+resource "aws_route_table" "private" {
+  count = 2
+
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.system_name}-${var.env_name}-private-route-table-${count.index}"
+  }
+}
+
+resource "aws_route" "private" {
+  count = 2
+
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw[0].id
+  route_table_id         = aws_route_table.private[count.index].id
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  route_table_id = aws_route_table.private[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
 }
