ab967628db69b009d42bf8a5da070ce3cab070a8
shonansurvivors <shonansurvivors@gmail.com>
2020-12-31 17:48:36 +0900

    EC2再作成を防ぐ

--- demo/terraform/ec2.tf
+++ demo/terraform/ec2.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "bastion" {
   ami           = "ami-01748a72bed07727c" // Amazon Linux 2 AMI (HVM), SSD Volume Type - ami-01748a72bed07727c (64-bit x86)
   instance_type = "t3.micro"
   key_name      = "${var.system_name}-${var.env_name}"
-  security_groups = [
+  subnet_id     = aws_subnet.public[0].id
+  vpc_security_group_ids = [
     aws_security_group.ssh.id
   ]
-  subnet_id = aws_subnet.public[0].id
 
   tags = {
     Name = "${var.system_name}-${var.env_name}-bastion"
