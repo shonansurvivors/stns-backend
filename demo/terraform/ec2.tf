75f475fd526f5d4bbf7a5330c46d0aa50b43397e
shonansurvivors <shonansurvivors@gmail.com>
2020-12-31 17:49:13 +0900

    install ansible

--- demo/terraform/ec2.tf
+++ demo/terraform/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   key_name      = "${var.system_name}-${var.env_name}"
   subnet_id     = aws_subnet.public[0].id
+  user_data     = file("./user_data/bastion.sh")
   vpc_security_group_ids = [
     aws_security_group.ssh.id
   ]
