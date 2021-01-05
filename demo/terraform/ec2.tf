25b122b35073e818a0dbe0a88945f4cbfd83cc7b
shonansurvivors <shonansurvivors@gmail.com>
2021-01-05 16:24:09 +0900

    create backend server

--- demo/terraform/ec2.tf
+++ demo/terraform/ec2.tf
@@ -13,3 +13,19 @@ resource "aws_instance" "bastion" {
     Name = "${var.system_name}-${var.env_name}-bastion"
   }
 }
+
+resource "aws_instance" "backend" {
+  count = 1
+
+  ami           = "ami-01748a72bed07727c" // Amazon Linux 2 AMI (HVM), SSD Volume Type - ami-01748a72bed07727c (64-bit x86)
+  instance_type = "t3.micro"
+  key_name      = "${var.system_name}-${var.env_name}"
+  subnet_id     = aws_subnet.private[0].id
+  vpc_security_group_ids = [
+    aws_security_group.vpc.id,
+  ]
+
+  tags = {
+    Name = "${var.system_name}-${var.env_name}-backend-${count.index}"
+  }
+}
